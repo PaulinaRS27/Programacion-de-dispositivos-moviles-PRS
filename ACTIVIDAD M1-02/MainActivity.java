4be8d0571986a40091f8956060c48d9f5f53e776
PaulinaRS27 <rodriguez1sam1pau@gmail.com>
2026-01-16 00:04:54 -0600

    ACTIVIDAD 2

---
+++ ACTIVIDAD M1-02/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.myapplication1;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+    //Aqui se coloca la etiqueta de busqueda en el logcat
+    private static final String TAG = "M102LogcatTag";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        //Mensaje que se va a imprimir en el logcat
+        Log.d(TAG, "¡Hello World desde Logcat!");
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
